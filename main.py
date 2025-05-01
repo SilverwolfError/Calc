d89d4c6f4fa299fc775fbb73c7e035bde867b077
Silverwolf_Error <125732803@qq.com>
2025-05-01 13:46:04 +0800

    Not try for Pyqt.

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import CalcAlpha03ForPyQt
 
 def click_addition():
-    print("点击了additionbutton按钮!!!!!")
+    if __name__ == '__main__':
+        app = QApplication(sys.argv)
+        MainWindow = QMainWindow()
+        ui = CalcAlpha03ForPyQt.Ui_Dialog()
+        ui.setupUi(MainWindow)
+        MainWindow.show()
+        ui.AdditionButton.clicked.connect(click_addition)
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
